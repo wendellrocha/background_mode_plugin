1ab7aefb782863977f93dae5bf7eae256a5dd75f
Wendell Rocha <wendellrocha@outlook.com>
2020-07-14 17:10:10 -0300

    release

--- ios/Classes/BackgroundModePlugin.h
+++ ios/Classes/BackgroundModePlugin.h
@@ -1,15 +1,4 @@
 #import <Flutter/Flutter.h>
-#import <AVFoundation/AVFoundation.h>
 
 @interface BackgroundModePlugin : NSObject <FlutterPlugin>
-{
-    AVAudioPlayer *audioPlayer;
-    BOOL enabled;
-}
-
-// Activate the background mode
-- (void)enable:(CDVInvokedUrlCommand *)command;
-// Deactivate the background mode
-- (void)disable:(CDVInvokedUrlCommand *)command;
-
 @end
